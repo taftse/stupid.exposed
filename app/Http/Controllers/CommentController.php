d19911a6a58d40e57a3f85b4464a5d72f43a08c4
Timothy Seebus <Timothyseebus@tools4schools.org>
2015-04-25 16:46:58 +0100

    added controller schaffholding

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,30 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class CommentController extends Controller {
+
+    /**
+     * Show the form for creating a new comment.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created comment in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+
+    }
+
+}
