7fcdea765272d3328c899ed8e06efe3f5fbda10c
Timothy Seebus <timothyseebus@tools4schools.org>
2020-04-06 19:31:16 +0100

    changes pre rewrite

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Event::listen('illuminate.query', function($query)
 Auth::routes();
 
 
-Route::get('home', 'HomeController@index');
+Route::get('/home', 'HomeController@index')->name('home');
 // list all posts
 Route::get('posts',['as' => 'posts', 'uses' =>'\SKE\Posts\Controllers\PostController@index']);
 Route::get('post', function(){
@@ -41,6 +41,6 @@ Route::get('category', function(){
 Route::get('category/{category}','\SKE\Categories\Controllers\CategoryController@show');
 Route::post('comment','\SKE\Comments\Controllers\CommentController@store');
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+
