411301a048cea5197bd0155ccf32a70dfe043218
Timothy Seebus <timothyseebus@tools4schools.org>
2020-04-07 17:19:47 +0100

    Update User Component

---
+++ SKE/Users/Http/Resources/AuthorResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SKE\User\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'link' =>[
+                'self' => route('user.show',$this->id),
+            ],
+        ];
+    }
+}
