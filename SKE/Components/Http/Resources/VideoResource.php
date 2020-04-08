fbbba9cf585d5a86ce84aa9fe7e0b23b5933d2b3
Timothy Seebus <timothyseebus@tools4schools.org>
2020-04-08 15:31:47 +0100

    update video resource

--- SKE/Components/Http/Resources/VideoResource.php
+++ SKE/Components/Http/Resources/VideoResource.php
@@ -14,6 +14,13 @@ class VideoResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'type'=>'video',
+            'provider'=>$this->provider,
+            'url'=> $this->when(!is_null($this->url),$this->url),
+            'embed_html'=> $this->when(!is_null($this->embed_html),$this->embed_html),
+            'embed_url'=> $this->when(!is_null($this->embed_url),$this->embed_url),
+            'metadata'=>$this->when(!is_null($this->metadata),$this->metadata),
+        ];
     }
 }
